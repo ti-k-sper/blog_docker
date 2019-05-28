84d997025abdb1403448e10db5a51f86e86f4f86
ti-k-sper <y.chargueraud@hotmail.fr>
2019-05-28 11:50:40 +0200

    remplissage table

--- createsql.php
+++ createsql.php
@@ -14,16 +14,37 @@ $pdo->exec('SET FOREIGN_KEY_CHECKS = 1');
 $faker = Faker\Factory::create('fr_FR');
 //dd($faker);
 
+$posts = [];
+$categories = [];
+
 for($i=0; $i<50; $i++){
     $pdo->exec("INSERT INTO post SET 
         name='{$faker->sentence()}', 
         slug='{$faker->slug}', 
         content='{$faker->paragraphs(rand(3, 15), true)}', 
         created_at='{$faker->date} {$faker->time}' ");
+    $posts[] = $pdo->lastInsertId();
 }
 
 for($i=0; $i<50; $i++){
     $pdo->exec("INSERT INTO category SET 
         name='{$faker->sentence(3, false)}', 
         slug='{$faker->slug}' ");
+    $categories[] = $pdo->lastInsertId();
+}
+
+foreach ($posts as $post) {
+    $randomCategories = $faker->randomElements($categories, 2);//dans la parenthese le chiffre correspond aux nombre de categorie pour 1 article
+    foreach ($randomCategories as $category) {
+        $pdo->exec("INSERT INTO post_category SET 
+                    post_id={$post}, 
+                    category_id={$category} ");
+    }
+}
+
+for($i=0; $i<50; $i++){
+    $password = password_hash($faker->password, PASSWORD_BCRYPT);
+    $pdo->exec("INSERT INTO user SET 
+        username='{$faker->name()}', 
+        password='$password' ");
 }
