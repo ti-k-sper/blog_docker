3776b111881c7faf0ffa5bddf9e08a358e827885
ti-k-sper <y.chargueraud@hotmail.fr>
2019-06-13 10:45:34 +0200

    config phpunit

--- www/tests/hello-world.php
+++
@@ -1,6 +0,0 @@
-<?php
-function helloWorld(string $name="World")
-{
-    //ucfirst -> 1ere lettre en maj
-    return 'Hello, '. ucfirst(strtolower($name)) . '!';
-}
--- www/tests/rna-transcription.php
+++
@@ -1,9 +0,0 @@
-<?php
-//
-// This is only a SKELETON file for the “Hamming” exercise. It’s been provided as a
-// convenience to get you started writing code faster.
-//
-function toRna($strand)
-{
-    return strtr($strand, 'GCTA', 'CGAU');//+++que str_replace
-}
