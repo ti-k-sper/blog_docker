bf6804ad7e41e9fdce4f1ba28a5cd891a213439e
ti-k-sper <y.chargueraud@hotmail.fr>
2019-06-24 10:58:16 +0200

    modif start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -e .env ]; then
+    source .env
+else
+    echo "Please set up your .env file before starting your environment."
+    exit 1
+fi
+
 docker-compose build
 
 docker-compose -f docker-compose.yml up -d
